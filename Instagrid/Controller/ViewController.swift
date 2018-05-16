dfdf6b052ad406f9b4b8f4b51ef373800800bc2a
t8lv4 <morgan@t8lv4.fr>
2018-05-16 17:22:33 +0200

    Add icons
    create groups
    rename swipe up and left images
    create outlets

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Morgan on 16/05/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topRectangle: UIButton!
+    @IBOutlet weak var bottomRectangle: UIButton!
+    @IBOutlet weak var topLeftSquare: UIButton!
+    @IBOutlet weak var topRightSquare: UIButton!
+    @IBOutlet weak var bottomLeftSquare: UIButton!
+    @IBOutlet weak var bottomRightSquare: UIButton!
+    
+    @IBOutlet weak var topRectangleLayout: UIButton!
+    @IBOutlet weak var bottomRectangleLayout: UIButton!
+    @IBOutlet weak var squaresLayout: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    //MARK: share
+
+    //MARK: swipe
+
+}
+
