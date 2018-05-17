9d8c9f35dd216b0ea04d4a92ec3f122f0100e196
t8lv4 <morgan@t8lv4.fr>
2018-05-17 15:19:26 +0200

    Add shadow to grids view
    add round corners to grids buttons
    create UIView+Shadow extension
    create UIView+Corner extension

---
+++ Instagrid/UIView+Corner.swift
@@ -0,0 +1,50 @@
+//
+//  UIView+Corner.swift
+//  Instagrid
+//
+//  Created by Morgan on 17/05/2018.
+//  Copyright © 2018 Morgan. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+
+}
